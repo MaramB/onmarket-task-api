eef09b28d4d97e7bea57c66f02fb51d1c2019ba3
MaramB <maram.badr1996@gmail.com>
2021-09-12 13:13:55 +0200

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+import { CustomersModule } from './customers/customers.module';
+import { OrdersModule } from './orders/orders.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,9 +14,10 @@ async function bootstrap() {
     .setTitle('On Market')
     .setDescription('On Market Task API')
     .setVersion('1.0')
-    .addTag('ecommerce')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [CustomersModule, OrdersModule], // don't include, say, BearsModule
+  });
   SwaggerModule.setup('', app, document);
 
   await app.listen(process.env.PORT || 3000);
